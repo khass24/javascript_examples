6d13e3af6db6a663d6ce31f12b0722cd6409fc18
Kyle Hassett <kylehassett72@gmail.com>
2018-10-11 21:28:54 -0500

    add callbacks

---
+++ js_callbacks/js_callback.js
@@ -0,0 +1,15 @@
+function firstTask() {
+  console.log("First Task Complete");
+}
+
+function secondTask() {
+  console.log("Second Task Complete");
+}
+
+function thirdTask() {
+  console.log("Third Task Complete");
+}
+
+setTimeout(firstTask, 2000);
+setTimeout(secondTask, 4000);
+setTimeout(thirdTask, 6000);
---
+++ js_callbacks/ruby_callback.rb
@@ -0,0 +1,13 @@
+def first_task
+  puts "First Task Complete"
+end
+
+def second_task
+  puts "Second Task Complete"
+end
+
+
+sleep 2
+first_task
+sleep 2 
+second_task
